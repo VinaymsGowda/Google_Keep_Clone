2569b59a655605f598a7ee64112c691e71fd3c5c
VinaymsGowda <vinaymsgowda27@gmail.com>
2023-11-07 18:41:41 +0530

    completed Crud Notes

--- api/routes/Noteapi.js
+++ api/routes/Noteapi.js
@@ -39,9 +39,43 @@ noteapi.post("/addnote",async(req,res)=>{
         });
         res.json(notedoc);
     })
-
-    
-
 });
+
+noteapi.put("/updatenote/:id",async(req,res)=>{
+    try {
+        const {title,content}=req.body;
+        const {id}=req.params;
+        const {token}=req.cookies;
+        jwt.verify(token,secret,{},async(err,info)=>{
+            if(err)
+                throw err;
+            const userid=info.id;
+            const updatenote=await NoteModel.updateOne({_id:id},{
+                title:title,
+                content:content,
+                author:userid,
+            });
+            res.json(updatenote);
+            console.log(updatenote);
+        });
+    } catch (error) {
+        res.status(404).json("Notes not found");
+    }
+})
+
+noteapi.delete("/deletenote/:id",async(req,res)=>{
+    try {
+        const {id}=req.params;
+        const deletenote=await NoteModel.deleteOne({_id:id});
+        if(deletenote.deletedCount==1){
+            res.json({deletenote,msg:"Deleted Note Successfully"});
+        }
+        else{
+            res.status(404).json("Invalid Note id doesnt exist");
+        }
+    } catch (error) {
+        res.status(404).json("Invalid Note id doesnt exist");
+    }
+})
 
 export default noteapi;
